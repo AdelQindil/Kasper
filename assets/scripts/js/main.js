dcc57478f92df96998590f0c0cbd5ee2fdf6b0fa
AdelQindil <adelqindil@outlook.com>
2022-11-15 17:19:00 +0200

    Toggle menu for mobile devices

--- assets/scripts/js/main.js
+++ assets/scripts/js/main.js
@@ -1,20 +1,34 @@
-/** @format */
-// Selectors
-const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-const rootElement = document.documentElement;
-const navIcon = document.getElementById("toggle-menu");
+// --Back to top button
+let toTopBtn = document.getElementById("scrollToTopBtn");
 
-// Functions
-const scrollToTop = () => {
-  // Scroll to top logic
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
+// *Show Back to top button when user scrolls away from top
+document.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 300) {
+    toTopBtn.classList.add("show-btn");
+  } else {
+    toTopBtn.classList.remove("show-btn");
+  }
+});
+
+// *Scroll to top function
+const toTop = () => {
+  document.documentElement.scrollIntoView();
 };
+
+// --Mobile navigation menu
+// Selectors
+const navIcon = document.getElementById("toggle-menu");
+const menu = document.querySelector("header nav ul");
+
+// *Toggle menu function
 const toggleBetweenClass = () => {
-  document.querySelector("header nav ul").classList.toggle("show-header-nav");
+  menu.classList.toggle("show-header-nav");
 };
-// EventListener
-scrollToTopBtn.addEventListener("click", scrollToTop);
+// Link the function to the toggle menu icon
 navIcon.addEventListener("click", toggleBetweenClass);
+// Close the toggle menu when user clicks anywhere outside
+document.addEventListener('click', function handleClickOutsideBox(e) {
+  if (!navIcon.contains(e.target)) {
+    menu.classList.remove("show-header-nav");
+  };
+});
